f8f522258490ae803a17c4faa505ed0b8cfd9c29
Isabela Müller <isabelamuller1@hotmail.com>
2022-01-30 23:01:01 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
             }
         }
         for (int i = 1; i <= 100; i++) {
-            if (i % 2 == 1) {
+            if (i % 2 != 0) {
                 System.out.println(i);
 
             }
